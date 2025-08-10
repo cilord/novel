bd97a5f8c3f2eace3951905d6fdef43f5e6556bc
cilord <146154004+cilord@users.noreply.github.com>
2025-08-10 17:02:00 +0000

    test pakai domain

--- scripts/sw.js
+++ scripts/sw.js
@@ -11,25 +11,27 @@ self.addEventListener('activate', event => {
 });
 
 self.addEventListener('fetch', event => {
-    console.log('fetching');
     // Memeriksa jika permintaan berasal dari domain yang ingin kita bypass
-    event.respondWith(
-        fetch(event.request)
-            .then(response => {
-                // Membuat respons baru dengan menambahkan header CORS
-                const newHeaders = new Headers(response.headers);
-                newHeaders.set('Access-Control-Allow-Origin', '*'); // Mengizinkan semua origin
+    if (event.request.url.startsWith('https://meionovels.com/')) {
+        console.log('test fetch');
+        event.respondWith(
+            fetch(event.request)
+                .then(response => {
+                    // Membuat respons baru dengan menambahkan header CORS
+                    const newHeaders = new Headers(response.headers);
+                    newHeaders.set('Access-Control-Allow-Origin', '*'); // Mengizinkan semua origin
 
-                const newResponse = new Response(response.body, {
-                    status: response.status,
-                    statusText: response.statusText,
-                    headers: newHeaders,
-                });
-                return newResponse;
-            })
-            .catch(error => {
-                console.error('Fetch gagal di Service Worker:', error);
-                return new Response('Fetch failed', { status: 500 });
-            })
-    );
+                    const newResponse = new Response(response.body, {
+                        status: response.status,
+                        statusText: response.statusText,
+                        headers: newHeaders,
+                    });
+                    return newResponse;
+                })
+                .catch(error => {
+                    console.error('Fetch gagal di Service Worker:', error);
+                    return new Response('Fetch failed', { status: 500 });
+                })
+        );
+    }
 });
