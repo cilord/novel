503c5dd3267c99e84f83103f7f4ebfd786909da3
cilord <146154004+cilord@users.noreply.github.com>
2025-08-10 17:13:39 +0000

    test

--- scripts/main.js
+++ scripts/main.js
@@ -31,7 +31,7 @@ function getContent(){
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('./scripts/sw.js')
+        navigator.serviceWorker.register('/sw.js')
             .then(registration => {
                 console.log('Service Worker berhasil didaftarkan: ', registration.scope);
             })
